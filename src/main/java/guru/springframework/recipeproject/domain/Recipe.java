da0fb65dea6980e81cfbc69434dbedde447d510c
Tasos Antoniou <tasos.an.antoniou@gmail.com>
2024-01-13 19:58:02 +0200

    Created some models and initialized h2

--- src/main/java/guru/springframework/recipeproject/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeproject/domain/Recipe.java
@@ -23,8 +23,16 @@ public class Recipe {
     @Lob
     private byte[] image;
 
+    @Enumerated(value = EnumType.STRING) //ordinal converts these values to numbers, so it is not recommended
+    private Difficulty difficulty;
+
     @OneToOne (cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -104,5 +112,29 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
